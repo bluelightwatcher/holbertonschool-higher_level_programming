d23efd16d0f2fb0257f36aac3ee9a1dcf18d423b
bluelightwatcher <9550@holbertonstudents.com>
2024-09-04 10:38:31 +0200

    Adding format string to print function

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -3,8 +3,8 @@ import random
 number = random.randint(-10, 10)
 
 if number == 0:
-    print("{number} is zero")
+    print("{} is zero".format(number))
 if number > 0:
-    print("{number} is positive")
+    print("{} is positive".format(number))
 if number < 0:
-    print("{number} is negative")
+    print("{} is negative".format(number))
