7b27208fd0a819651e36abb19f42cac98dc6489b
bluelightwatcher <9550@holbertonstudents.com>
2024-09-05 16:13:36 +0200

    Program prints all the calculation in one instance

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -5,8 +5,11 @@ a = 10
 b = 5
 
 if __name__ == "__main__":
-    # Perform the operations and print the results
-    print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".format(a, b, sub(a, b)))
-    print("{} * {} = {}".format(a, b, mul(a, b)))
-    print("{} / {} = {}".format(a, b, div(a, b)))
+
+    result_add = add(a, b)
+    result_sub = sub(a, b)
+    result_mul = mul(a, b)
+    result_div = div(a, b)
+   
+    print("{} + {} = {}\n{} - {} = {}\n{} * {} = {}\n{} / {} = {}".format(
+        a, b, result_add, a, b, result_sub, a, b, result_mul, a, b, result_div))
