9242e025f9f1825f240d4ad8d5865b7a9ec0a194
bluelightwatcher <9550@holbertonstudents.com>
2024-09-06 10:08:42 +0200

    Program uses sys modules to parse arguments

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -10,6 +10,5 @@ if __name__ == "__main__":
     result_sub = sub(a, b)
     result_mul = mul(a, b)
     result_div = div(a, b)
-   
     print("{} + {} = {}\n{} - {} = {}\n{} * {} = {}\n{} / {} = {}".format(
         a, b, result_add, a, b, result_sub, a, b, result_mul, a, b, result_div))
