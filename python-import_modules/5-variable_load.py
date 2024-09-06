c71d6e7350234a4bb9f04bf361d9d935f142060f
bluelightwatcher <9550@holbertonstudents.com>
2024-09-06 10:38:40 +0200

    Adding print debugging

--- python-import_modules/5-variable_load.py
+++ python-import_modules/5-variable_load.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
-from variable_load_5.py import a
+from variable_load_5. import a
 
 
 def main():
+    print(f"main func starts now")
     print("{}".format(a))
 
     if __name__ == "__main__":
