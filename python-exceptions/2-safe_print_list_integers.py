2d27843561dbf16d1d202c56ebea109010a7b24d
bluelightwatcher <9550@holbertonstudents.com>
2024-09-11 15:51:51 +0200

    Handling the print exception from outside the loop

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -8,7 +8,7 @@ def safe_print_list(my_list=[], x=0):
             count += 1
         except (ValueError, TypeError):
             continue
-        except IndexError:
-            break
+    except IndexError:
+        pass 
     print()
     return count
