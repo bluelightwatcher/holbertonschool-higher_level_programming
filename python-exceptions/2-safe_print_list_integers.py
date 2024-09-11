1935c668543b37d5ff96456de7af87c4cfb5cdc1
bluelightwatcher <9550@holbertonstudents.com>
2024-09-11 15:54:30 +0200

    Done by AI

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 
-def safe_print_list(my_list=[], x=0):
-    count = 0
-    for i in range(x):
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0  # To track the number of integers printed
+    for i in range(x):  # Loop over the first 'x' elements
         try:
-            print("{:d}".format(my_list[i]), end="")
-            count += 1
-        except (ValueError, TypeError):
+            print("{:d}".format(my_list[i]), end="")  # Print integer elements
+            count += 1  # Increment count if integer is printed
+        except (ValueError, TypeError):  # Skip non-integer elements
             continue
-    except IndexError:
-        pass 
-    print()
-    return count
+        except IndexError:  # Handle case where 'x' exceeds list length
+            break
+    print()  # Newline after printing all valid integers
+    return count  # Return the number of integers printed
