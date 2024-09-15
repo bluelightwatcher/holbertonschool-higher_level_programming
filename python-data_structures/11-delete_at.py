161ff60af9b83231668124bf34c8cb9c1a85e2b3
bluelightwatcher <e.seguin2023@gmail.com>
2024-09-15 13:35:43 +0200

    Update 11-delete_at.py

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-a = 89
-b = 10
-a, b = b, a  # Switching values
-print("a={:d} - b={:d}".format(a, b))
+def delete_at(my_list=[], idx=0):
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    del my_list[idx]
+    return my_list
