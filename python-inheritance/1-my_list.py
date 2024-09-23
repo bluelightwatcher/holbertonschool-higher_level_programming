75c20313fda5acbd7c6c63a721bf67153205be87
bluelightwatcher <9550@holbertonstudents.com>
2024-09-23 15:44:20 +0200

    Module sorts a list

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
+""" This modules sorts a list"""
 
 class Mylist(list):
+    """ This class is a child of the list class
+
+    Args: 
+        list: is the parent class
+
+    Returns:
+        A sorted list
+    """
+
     def __init__(self):
         super().__init__(self)
 
         sort(Mylist)
-        print(Mylist)
-
-    
+        print(Mylist)   
