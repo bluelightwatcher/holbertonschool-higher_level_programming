ca5703fbfb983ed70da0a8145a1dfa9971da08f5
bluelightwatcher <9550@holbertonstudents.com>
2024-09-23 16:18:20 +0200

    Documentation should be correct

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,22 +1,16 @@
 #!/usr/bin/python3
 """ This modules sorts a list"""
 
+
 class Mylist(list):
     """ This class is a child of the list class
-
-    Attributes: 
-        none
     """
 
     def __init__(self):
         """ Function sorts a list
-
-        args:
-            none
         Returns:
             sorted list
         """
         super().__init__(self)
         self.sort()
-
-        print(self)   
+        print(self)
