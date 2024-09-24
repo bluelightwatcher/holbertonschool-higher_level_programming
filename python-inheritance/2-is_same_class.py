b3494273ec438fd6e4d54328d48298e6f50b90f4
bluelightwatcher <9550@holbertonstudents.com>
2024-09-24 15:00:49 +0200

    Using __eq__ method on obj and classes

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This module prints all attributes of a class in a list"""
+
+
+def is_same_class(obj, a_class)
+
+
+"""
+    Function uses eq magic method
+
+    Method:
+        __eq_: magic method compares two objects
+
+    Args:
+        obj: is the object to compare
+        a_class: is the class to compare to
+
+    Returns: boolean
+    """
+return obj == a_class
