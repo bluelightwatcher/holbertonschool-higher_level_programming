682ab521dae2fae69d394cf7621793266984cf46
bluelightwatcher <9550@holbertonstudents.com>
2024-09-24 15:52:46 +0200

    Replacing the last condition

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -17,5 +17,7 @@ def inherits_from(obj, a_class):
         Returns:
             boolean
     """
-    if isinstance(obj, a_class): 
+    if isinstance(obj, a_class) and type(obj) is not a_class: 
         return True
+    else:
+        return False
