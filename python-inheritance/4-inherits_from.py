27fdad8e7ea9043ddd9d98196706028030faab67
bluelightwatcher <9550@holbertonstudents.com>
2024-09-24 15:47:57 +0200

    Programm returns true if object is instance of a subclass and not the class itself

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This module compares object"""
+
+
+def inherits_from(obj, a_class):
+
+    """
+    Function uses eq magic method
+
+        Method:
+            __eq_: magic method compares two objects
+
+        Args:
+            obj: is the object to compare
+            a_class: is the class to compare to
+
+        Returns:
+            boolean
+    """
+    if isinstance(obj, a_class) and type(obj) is not a_class:
+        return True
+        return False
