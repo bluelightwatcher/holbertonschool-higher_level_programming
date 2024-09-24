8c723c24adf0572d6299dcf9078e37ca07379a57
bluelightwatcher <9550@holbertonstudents.com>
2024-09-24 15:16:42 +0200

    Module uses if statement for comparison

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
-"""This module prints all attributes of a class in a list"""
+"""This module compares object"""
 
 
-def is_same_class(obj, a_class)
+def is_same_class(obj, a_class):
 
 
-"""
+    """
     Function uses eq magic method
 
-    Method:
-        __eq_: magic method compares two objects
+        Method:
+            __eq_: magic method compares two objects
 
-    Args:
-        obj: is the object to compare
-        a_class: is the class to compare to
+        Args:
+            obj: is the object to compare
+            a_class: is the class to compare to
 
-    Returns: boolean
+        Returns:
+            boolean
     """
-return obj == a_class
+    if isinstance(obj, a_class):
+        return True
+    return False
