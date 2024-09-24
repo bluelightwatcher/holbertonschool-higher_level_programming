c11dfe33376ae68f1465f7e697667cc50dc77707
bluelightwatcher <9550@holbertonstudents.com>
2024-09-24 15:49:35 +0200

    Removing one condition

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -17,6 +17,5 @@ def inherits_from(obj, a_class):
         Returns:
             boolean
     """
-    if isinstance(obj, a_class) and type(obj) is not a_class:
+    if isinstance(obj, a_class): 
         return True
-        return False
