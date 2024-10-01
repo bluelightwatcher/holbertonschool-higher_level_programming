f1761aae9a23facb1584be8bec92e9da6307499a
bluelightwatcher <9550@holbertonstudents.com>
2024-10-01 13:41:52 +0200

    Module returns JSON encoded file

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import json
+"""module returns the json repr of a string"""
+
+
+def to_json_string(my_obj):
+    """function returns type of a json object
+
+    Args:
+        my_obj
+    Return:
+        my_obj: type of obj
+    """
+    my_objJSON = json.dumps(my_obj)
+    return (my_objJSON)
