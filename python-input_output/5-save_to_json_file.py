93d11c537a6b5a4305073eccffa24fa1fa583eae
bluelightwatcher <9550@holbertonstudents.com>
2024-10-03 13:54:57 +0200

    Does not create file but returns the type error

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+"""Module writes a json string to a text file."""
 import json
 
+
 def save_to_json_file(my_obj, filename):
-    """Writes an object to a text file, using a JSON representation.
+    """Write an object to a text file, using a JSON representation.
     
     Args:
         my_obj: The Python object to be serialized and saved.
@@ -10,4 +12,4 @@ def save_to_json_file(my_obj, filename):
     """
     # Use the 'with' statement correctly, without any 'if'
     with open(filename, 'w', encoding='utf-8') as file:
-        json.dump(my_obj, file)        
+        file = json.dumps(my_obj)
