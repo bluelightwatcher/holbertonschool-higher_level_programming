0bfbcbea65e6237fed9e7558b54bb6edebf38479
bluelightwatcher <9550@holbertonstudents.com>
2024-10-31 22:36:53 +0100

    Flushing privileges to grant user straigt away

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
 -- script create a root user like user
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
