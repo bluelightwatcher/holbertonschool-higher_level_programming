e153058e104873201638e1026bb2554014d1c3a3
bluelightwatcher <9550@holbertonstudents.com>
2024-11-01 21:41:35 +0100

    Using Removing equal to california and group by statement

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -2,6 +2,4 @@
 SELECT cities.id, cities.name, states.name
 FROM cities
 INNER JOIN states ON cities.state_id = states.id
-WHERE states.name = 'California'
-GROUP BY cities.id, cities.name, states.name
 ORDER BY cities.id ASC;
