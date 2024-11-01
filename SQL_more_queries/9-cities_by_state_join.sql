43bb3d8dc5e87c2e9071319a59dcfec9ab9791b8
bluelightwatcher <9550@holbertonstudents.com>
2024-11-01 21:34:26 +0100

    Using LEFT join

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,7 @@
+-- module will use join and not view
+SELECT cities.id, cities.name, states.name
+FROM cities
+LEFT JOIN states ON cities.state_id = states.id
+WHERE states.name = 'California'
+GROUP BY cities.id, cities.name, states.name
+ORDER BY cities.id ASC;
