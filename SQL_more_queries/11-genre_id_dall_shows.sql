5b10cf5097c8a1eb8f744ad13e4efab6858ff723
bluelightwatcher <9550@holbertonstudents.com>
2024-11-02 11:51:59 +0100

    Using left join to display all data in the tv_show table

---
+++ SQL_more_queries/11-genre_id_dall_shows.sql
@@ -0,0 +1,5 @@
+-- using join to display data from two tables
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
