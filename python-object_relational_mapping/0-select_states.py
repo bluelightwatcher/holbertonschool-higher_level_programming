d6d98068b141ac285f37f56d9a34c5efcf99f0d2
bluelightwatcher <9550@holbertonstudents.com>
2024-11-03 15:13:07 +0100

    Module is documented

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" This module uses MySQLdb to write sql query
+using the sys module to gather data from the CLI
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    """ Using sys module"""
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    """Database settings"""
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    """implementing cursor"""
+    cursor = db.cursor()
+
+    """ Execute SQL query in SQL"""
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    """ retrieving all rows with cursor"""
+    rows = cursor.fetchall()
+
+    """printing the row""" 
+    for row in rows:
+        print(row)
+
+    """ closing cursor and database"""
+    cursor.close()
+    db.close()
