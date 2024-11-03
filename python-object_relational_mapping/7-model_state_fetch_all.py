b5205bf006b9db68a66d6b795566d8f476481547
bluelightwatcher <9550@holbertonstudents.com>
2024-11-03 17:34:38 +0100

    Using alchemy

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,16 +1,35 @@
-#!/usr/bin/python3
+# 7-model_state_fetch_all.py
+import sys
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State, Base
-import sys
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    # Check that the correct number of arguments is provided
+    if len(sys.argv) != 4:
+        print("Usage: ./7-model_state_fetch_all.py <mysql username> <mysql password> <database name>")
+        sys.exit(1)
+
+    # Get the command line arguments
+    mysql_user = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    # Create the engine
+    engine = create_engine(f'mysql+mysqldb://{mysql_user}:{mysql_password}@localhost/{database_name}', pool_pre_ping=True)
+
+    # Create a configured "Session" class
     Session = sessionmaker(bind=engine)
+
+    # Create a session
     session = Session()
 
+    # Query all State objects sorted by states.id
     states = session.query(State).order_by(State.id).all()
-    for state in states:
-        print("{}: {}".format(state.id, state.name))
 
+    # Print the results
+    for state in states:
+        print(f"{state.id}: {state.name}")
+
+    # Close the session
     session.close()
